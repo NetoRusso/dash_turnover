2a5aa08246242dd03ae2db56dc36e726cbee0d9d
Neto Russo <netoacrn1@gmail.com>
2024-07-23 20:13:24 -0300

    mergin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import logo from './logo_Turnover_branco.png';
 import './styles/App.css';
-=======
-import './App.css';
->>>>>>> a41487700f20963abdde09424b41ba5ebf319b31
 
 function App() {
   return (
