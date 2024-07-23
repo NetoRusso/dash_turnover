60f0d39bdb26eb760653616df9f300715abba291
Neto Russo <netoacrn1@gmail.com>
2024-07-23 19:44:18 -0300

    Subindo o app base

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo_Turnover_branco.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className='logoTurnover' alt="Logo da Turnover" />
+        <p>
+          #BORACODAR
+        </p>
+          
+      </header>
+    </div>
+  );
+}
+
+export default App;
