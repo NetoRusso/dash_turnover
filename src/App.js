a41487700f20963abdde09424b41ba5ebf319b31
Milena <milenaleme4@hotmail.com>
2024-07-23 23:12:41 -0300

    start projeto

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import logo from './logo_Turnover_branco.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className='logoTurnover' alt="Logo da Turnover" />
-        <p>
-          #BORACODAR
-        </p>
-          
-      </header>
-    </div>
+    <div></div>
   );
 }
 
